2180d50fd126a30bcbfaa3b70ea6e138b2af6ee6
Jose luis Hernandez <nubelibreweb@gmail.com>
2026-09-09 11:12:54 -0500

    Organizar y excluir documentos

--- JOSE_LUIS_HERNANDEZ_AA5_EV03/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Redirección al punto de entrada en la carpeta public
-header("Location: public/index.php");
-exit();
--- JOSE_LUIS_HERNANDEZ_AA5_EV04/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Redirección al punto de entrada principal del módulo
-header("Location: products/list.php");
-exit();
